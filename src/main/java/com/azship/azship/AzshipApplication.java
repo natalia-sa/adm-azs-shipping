9dab9878fe6fca33f900f794fb7041831d2accc1
Natalia Salvino <nataliasalvino86@gmail.com>
2024-04-10 16:38:02 -0300

    chore: initializing project

---
+++ src/main/java/com/azship/azship/AzshipApplication.java
@@ -0,0 +1,13 @@
+package com.azship.azship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azship/azship/AzshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azship.azship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
