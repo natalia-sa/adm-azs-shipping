7cc09772b01f4a814123ed1aafd93accea713de8
Natalia Salvino <nataliasalvino86@gmail.com>
2024-04-10 22:31:52 -0300

    feat: adding POST, GET, DELETE and PUT api endpoints

--- src/main/java/com/azship/azship/FreightApiController.java
+++ src/main/java/com/azship/azship/FreightApiController.java
@@ -5,9 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/freight")
@@ -16,9 +14,33 @@ public class FreightApiController {
     @PostMapping(value = "")
     @Operation(summary = "Save new freight")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "The saved freight")})
+            @ApiResponse(responseCode = "201", description = "Freight was saved successfully")})
     public ResponseEntity save() {
         return new ResponseEntity<>("Done", HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "")
+    @Operation(summary = "Update freight")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Freight was updated successfully")})
+    public ResponseEntity update() {
+        return new ResponseEntity<>("Done", HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "")
+    @Operation(summary = "Delete freight")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Freight was deleted successfully")})
+    public ResponseEntity delete() {
+        return new ResponseEntity<>("Done", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "")
+    @Operation(summary = "List freights")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The freights found")})
+    public ResponseEntity findByPagination() {
+        return new ResponseEntity<>("Done", HttpStatus.OK);
     }
 
 }
